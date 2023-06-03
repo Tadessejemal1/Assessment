d21a2ded4265179dc2504de8afa1d34b5d7b2700
Tadesse Jemal <tadessejemal63@gmail.com>
2023-06-03 13:32:39 +0300

    import components inside this

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SideBar from './Components/SideBar';
+import Main from './Components/Main';
+import Box from '@mui/material/Box';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: 'flex',gap:'20px' }}>
+      <SideBar /> 
+      <Main />
+    </Box>
   );
-}
+};
 
 export default App;
